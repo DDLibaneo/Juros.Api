1502e13a65667e199a4dee86f335e27116f78911
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2022-02-25 18:02:58 -0300

    wip

---
+++ Juros.DataStore/Operations/Commands.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Juros.DataStore.Queries
+{
+    public interface ICommands 
+    { 
+    
+    }
+
+    public class Commands
+    {
+
+    }
+}
---
+++ Juros.DataStore/Operations/Queries.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Juros.DataStore.Operations
+{
+    public interface IQueries
+    {
+
+    }
+
+    public class Queries
+    {
+
+    }
+}
