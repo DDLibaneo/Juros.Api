8b57b78cecf90138b64eaf4b9ce9142ca97d4c0f
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2022-02-26 14:52:56 -0300

    add wip

---
+++ Juros.Models/Dtos/JuroDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Juros.Models.Dtos
+{
+    public class JuroDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d+(\.\d{1,2})?$")]
+        [Range(0, 100)]
+        public decimal Taxa { get; set; }
+    }
+}
---
+++ Juros.Models/Entities/Juro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace Juros.Models.Entities
+{
+    public class Juro
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d+(\.\d{1,2})?$")]
+        [Range(0, 100)]
+        public decimal Taxa { get; set; }
+    }
+}
