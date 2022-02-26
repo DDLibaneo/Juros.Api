f004a69ca89f260d529f6d49a498af211c2a68fc
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2022-02-26 20:40:18 -0300

    wip service tests

---
+++ Juros.Services.Tests/JurosServiceTests.cs
@@ -0,0 +1,19 @@
+using Juros.DataStore.Operations;
+using Moq;
+using System;
+using Xunit;
+
+namespace Juros.Services.Tests
+{
+    public class JurosServiceTests
+    {
+        private readonly JurosService _service;
+        private readonly Mock<ICommands> _commands = new Mock<ICommands>();
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
