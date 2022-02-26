c063fb271dea787a844cef742b5e36cfb88de4ab
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2022-02-25 23:09:06 -0300

    wip

--- Juros.Api.Integration.Tests/TestStartup.cs
+++ Juros.Api.Integration.Tests/TestStartup.cs
@@ -1,9 +1,24 @@
-﻿
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Juros.DataStore;
 
 namespace Juros.Api.Integration.Tests
 {
     public class TestStartup : Startup
     {
+        public TestStartup(IConfiguration configuration) : base(configuration)
+        { }
 
+        protected override void ConfigureDb(IServiceCollection services)
+        {
+            var controllersAssemblyType = typeof(Startup).GetTypeInfo().Assembly;
+            services.AddMvc().AddApplicationPart(controllersAssemblyType);
+
+            var serviceProvider = services.BuildServiceProvider();
+            var options = serviceProvider.GetService<DbContextOptions<JurosDbContext>>();
+            services.AddScoped(s => new JurosDbContext(options));
+        }
     }
 }
