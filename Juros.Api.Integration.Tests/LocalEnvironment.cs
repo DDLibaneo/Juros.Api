2f6437b8f2c3924cc1f1705e07ba67bd107183aa
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2022-02-25 21:15:01 -0300

    wip

--- Juros.Api.Integration.Tests/LocalEnvironment.cs
+++ Juros.Api.Integration.Tests/LocalEnvironment.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.TestHost;
+﻿using Juros.DataStore;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
 using System.Net.Http;
 
 namespace Juros.Api.Integration.Tests
@@ -6,8 +8,17 @@ namespace Juros.Api.Integration.Tests
     internal class LocalEnvironment : IEnvironment
     {
         public HttpClient Client { get; }
+        public JurosDbContext JurosDbContext { get; internal set; }
 
         private readonly TestServer _server;
-        //private readonly DbContextOptions<>
+        private readonly DbContextOptions<JurosDbContext> _options;        
+
+        public LocalEnvironment()
+        {
+            var connection = new SqlLiteConnection("DataSource=:memory:");
+            connection.Open();
+
+
+        }
     }
 }
--- Juros.DataStore/JurosDbContext.cs
+++ Juros.DataStore/JurosDbContext.cs
@@ -1,9 +1,17 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.SqlServer;
+using System;
+using Microsoft.EntityFrameworkCore;
 
 namespace Juros.DataStore
 {
-    public class JurosDbContext 
+    public class JurosDbContext : DbContext
     {
+        public JurosDbContext(DbContextOptions<JurosDbContext> options) 
+            : base(options) { }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }        
     }
 }
