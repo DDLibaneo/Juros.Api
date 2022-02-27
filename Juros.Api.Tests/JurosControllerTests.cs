e1686b5dc9155769937ab8364cef5ad7efd0cb7e
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2022-02-27 15:27:46 -0300

    wip controllerTests

--- Juros.Api.Tests/JurosControllerTests.cs
+++ Juros.Api.Tests/JurosControllerTests.cs
@@ -1,16 +1,20 @@
 using System;
 using Xunit;
 using System.Threading.Tasks;
+using Juros.Api.Controllers;
+using Moq;
+using Juros.Services;
 
 namespace Juros.Api.Tests
 {
     public class JurosControllerTests
     {
-        private readonly 
+        private readonly JurosController _jurosController;
+        private readonly Mock<IJurosService> _jurosService = new Mock<IJurosService>();
 
-        public JurosControllerTests()
+        public JurosControllerTests(JurosController jurosController)
         {
-
+            _jurosController = jurosController;
         }
 
         [Fact(DisplayName = "CreateJuro - [Success] - Returns success status code and Juro Id")]
